680376453cc4af5beeb26e0f757ddb93b0c81cb3
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-04-25 02:51:09 +0600

    Breaking News Dynamic

--- src/components/shared/BreakingNews.jsx
+++ src/components/shared/BreakingNews.jsx
@@ -1,70 +1,28 @@
+import Link from "next/link";
 import Marquee from "react-fast-marquee";
 
-const BreakingNews = () => {
+const BreakingNews = async () => {
+  const res = await fetch(
+    "https://69ebd41b97482ad5c52814b9.mockapi.io/breakingNews",
+    { cache: "no-store" },
+  );
+  const datas = await res.json();
+  // console.log(datas);
   return (
     <div className="container mt-4 mx-auto py-2 bg-gray-200 rounded-lg flex justify-center items-center px-4">
       <button className="text-2xl bg-red-500 px-4 py-2 rounded-r-lg">
         Latest
       </button>
-      <Marquee pauseOnHover={true}>
-        <div className="text-foreground bg-background py-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quae
-          temporibus impedit laudantium, distinctio molestias non, consequatur
-          harum unde debitis minima adipisci quidem omnis accusamus architecto
-          reprehenderit. Aut, culpa? Veritatis consequuntur officia totam vitae
-          aut. Beatae obcaecati cumque ut? Rem ipsam esse eius animi quaerat sed
-          amet sit quam, suscipit necessitatibus, expedita accusamus, doloremque
-          tenetur ratione alias. Iste dolorem itaque doloremque est deserunt
-          vitae rem consequuntur impedit eius quos magni placeat esse, ipsum
-          similique pariatur unde commodi quis aliquid dolore quibusdam modi
-          minima quasi voluptates temporibus. Nostrum laboriosam pariatur illum
-          blanditiis repellendus accusamus, delectus minus eius optio magni
-          deserunt dicta dolorum sapiente. Commodi eum dolorum corrupti, quae
-          natus quas autem quaerat delectus at. Provident adipisci, voluptatibus
-          obcaecati sequi optio, saepe ratione dignissimos nemo a, error
-          distinctio esse cupiditate sunt. Adipisci, ipsam quia? Laboriosam ut
-          voluptas optio debitis? Quae distinctio inventore expedita quos
-          provident exercitationem non enim necessitatibus. Iste eum officiis
-          architecto quo eveniet, perspiciatis necessitatibus hic? Ea repellat
-          hic autem amet illo, aliquid ratione! Aliquid amet harum voluptate
-          ratione fugiat labore sunt eius odit at? Maiores iusto accusantium
-          natus maxime unde adipisci velit qui pariatur voluptatum nisi error,
-          consequatur ea debitis dolore perferendis, expedita fuga amet
-          excepturi impedit eius! At, provident impedit earum dolore odit maxime
-          asperiores praesentium consectetur error, labore, ipsam placeat esse
-          nobis magni minima voluptas id totam fuga ipsum amet? Explicabo,
-          consequuntur illum. Non tenetur quis illo autem maiores quod, fuga
-          placeat sed corporis, id numquam, omnis ea? Itaque eaque sit in
-          dignissimos ad debitis optio, at amet eligendi ex exercitationem illum
-          ea perspiciatis, quae ipsam id asperiores nobis nisi nostrum
-          explicabo. Dolorum praesentium ipsa atque iusto quos quas dolores
-          totam voluptatem eos quam maxime accusantium soluta sequi nesciunt
-          molestiae facere, assumenda error. Saepe doloribus laboriosam
-          necessitatibus optio reiciendis non tenetur qui odit aut quae
-          voluptatum quasi, dolore, cupiditate unde ipsum dignissimos sequi
-          minus debitis labore voluptatibus modi, placeat totam? Quasi, nostrum
-          quibusdam ipsam suscipit quia reiciendis amet inventore. Quia ad
-          laborum ipsa voluptatibus aspernatur obcaecati, eaque rem deserunt,
-          illum, nihil nesciunt? A beatae praesentium illo nam corrupti libero
-          pariatur cumque maiores porro expedita ipsam dolorum, incidunt hic
-          quas. Ipsum corrupti ipsa delectus tempora, non molestiae, odio iure
-          similique adipisci expedita repellendus, magnam impedit voluptatibus
-          sunt tenetur debitis. Reprehenderit voluptatum voluptate sint iure
-          dicta deserunt nulla dolorem non repellat illum maiores, placeat odit
-          vitae dolores quasi maxime? Officiis, eligendi laboriosam? Aliquam,
-          error assumenda ea obcaecati aspernatur totam eligendi libero nam
-          natus vero animi, explicabo nobis optio beatae illum accusantium
-          suscipit, numquam incidunt. Quasi eum quam rem facere consequuntur
-          error recusandae! Laboriosam dolore accusantium, commodi error cum
-          ipsum voluptate architecto praesentium id culpa accusamus
-          reprehenderit officia, eveniet ratione quidem nostrum beatae natus
-          iusto nobis velit repellat. Architecto quasi fuga ex possimus vitae ut
-          esse obcaecati rerum id. Rerum exercitationem, voluptate, ipsa esse
-          odio adipisci doloribus perferendis porro ut dolore sed nulla
-          laboriosam excepturi itaque corrupti voluptatibus illo quae illum?
-          Nemo, eligendi. Similique modi, repellat nisi a magni amet inventore
-          esse, officiis facilis quia rerum architecto excepturi libero earum!
-        </div>
+      <Marquee className="overflow-y-hidden" pauseOnClick={true}>
+        {datas.map((data, ind) => (
+          <Link
+            className="text-foreground  hover:underline mr-2 bg-background py-2"
+            key={ind}
+            href={`/news/${data.id}`}
+          >
+            {data.title}
+          </Link>
+        ))}
       </Marquee>
     </div>
   );
