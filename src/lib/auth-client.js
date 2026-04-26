54991846b0fd11d74f3e630c1445a9654c098c37
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-04-27 00:39:20 +0600

    Update some  ui

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/client";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  baseURL: "https://global-news-time.vercel.app/",
 });
 
 export const { signIn, signUp, signOut, getSession, useSession } = authClient;
